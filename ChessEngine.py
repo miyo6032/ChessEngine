38ab43ccf528c6a7aa883ad67757b780bc420688
miyo6032 <miyo6032@colorado.edu>
2018-12-01 13:59:13 -0700

    Added alpha beta pruning

--- ChessEngine.py
+++ ChessEngine.py
@@ -27,35 +27,45 @@ class ChessEngine():
         self.board.push(move)
 
     def GetMove(self, depth):
-        max_score = float('-inf')
+        alpha = float('-inf')
+        beta = float('inf')
         best_move = None
 
         # Find the best move
         for move in self.board.legal_moves:
             self.board.push(move)
             # The best move is based on future moves of the player and enemy
-            score = - self.Negamax(depth - 1)
-            if score > max_score:
-                max_score = score
+            score = - self.Negamax(depth - 1, -alpha, -beta)
+            if score > alpha:
+                alpha = score
                 best_move = move
             self.board.pop()
         return best_move
 
-    def Negamax(self, depth):
+    # Algorithm based on pseudocode from https://www.chessprogramming.org/Alpha-Beta
+    # This is a 'soft fail' alpha beta that allows scores outside the alpha beta bounds
+    # Further reading: https://www.chessprogramming.org/Fail-Soft
+    def Negamax(self, depth, alpha, beta):
         # Reach the end of the tree
         if depth == 0:
             return self.Evaluate()
-        max_score = float('-inf')
 
+        best_value = float('-inf')
         # Find the best move
         for move in self.board.legal_moves:
             self.board.push(move)
             # The best move is based on future moves of the player and enemy
-            score = - self.Negamax(depth - 1)
-            if score > max_score:
-                max_score = score
+            score = - self.Negamax(depth - 1, -alpha, -beta)
+            if score >= beta:
+                self.board.pop()
+                return score
+
+            if score > best_value:
+                best_value = score
+                if score > alpha:
+                    alpha = score
             self.board.pop()
-        return max_score
+        return alpha
 
     def Evaluate(self):
         piece_map = self.board.piece_map()
@@ -82,6 +92,8 @@ class ChessEngine():
 
 chess_engine = ChessEngine()
 chess_engine.PrintBoard()
+
+# Test chess playing: (right now it's really stupid)
 for i in range(50):
-    chess_engine.Move(chess_engine.GetMove(2))
+    chess_engine.Move(chess_engine.GetMove(5))
     chess_engine.PrintBoard()
